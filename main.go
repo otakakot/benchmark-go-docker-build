9b754ff1627ce064535cbac154b5b25e9083d3c1
otakakot <117251387+otakakot@users.noreply.github.com>
2026-07-20 13:30:45 +0900

    first commit

--- main.go
+++ main.go
@@ -9,16 +9,8 @@ import (
 func main() {
 	engine := gin.Default()
 
-	engine.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, "ok")
-	})
-
 	engine.GET("/health", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, "ok")
-	})
-
-	engine.GET("/ping", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, "pong")
 	})
 
 	if err := engine.Run(); err != nil {
