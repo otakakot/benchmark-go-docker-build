6d0468e918e48e12c9d720812c314512dcc5475e
otakakot <117251387+otakakot@users.noreply.github.com>
2026-07-20 16:39:36 +0900

    first commit

--- main.go
+++ main.go
@@ -4,12 +4,21 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func main() {
+	logger, err := zap.NewProduction()
+	if err != nil {
+		panic(err)
+	}
+	defer logger.Sync()
+
 	engine := gin.Default()
 
 	engine.GET("/health", func(ctx *gin.Context) {
+		logger.Info("handling request", zap.String("path", "/health"))
+
 		ctx.JSON(http.StatusOK, "ok")
 	})
 
